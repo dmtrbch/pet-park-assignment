7dde0423b3e68b2e344a3ac538810c1e41669d8e
dmtrbch <bochvarovskidimitar@gmail.com>
2023-08-02 20:24:41 +0200

    initial commit

--- src/PetPark.sol
+++ src/PetPark.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.13;
 
 contract PetPark {
-    
+    enum AnimalType {
+        Fish,
+        Cat,
+        Dog,
+        Rabbit,
+        Parrot
+    }
+
+    event Aded(AnimalType animalType, uint256 animalCount);
+
+    function add() external {}
 }
